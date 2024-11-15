faeb1370b20f95dabef6511ff8f980f24c41a3e1
Richard Trofimov <116758171+riccorichards@users.noreply.github.com>
2024-11-15 20:31:26 +0400

    Update cors app.js

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
+const corsOptions = {
+  origin: "http://localhost:5173", // URL of your Vite React app
+  methods: ["GET", "POST", "PUT", "DELETE"], // Allowed HTTP methods
+  allowedHeaders: ["Content-Type", "Authorization"], // Allowed headers
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 mongo_uri =
   "mongodb+srv://trriccorichards:14dEn8ymHvN2OH4q@cluster0.ojuvf.mongodb.net/fakedataforpdf?retryWrites=true&w=majority&appName=Cluster0";
